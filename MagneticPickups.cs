456026beb17458b2b4547d1ced276d5a8e481822
ruudschouten <ruud.schouten@outlook.com>
2023-03-30 11:00:50 +0200

    Tidied up code a little bit

--- MagneticPickups.cs
+++ MagneticPickups.cs
@@ -10,7 +10,6 @@ namespace MagneticPickups
     [BepInPlugin("com.blappole.magneticpickups", "Magnetic Pickups", "1.1.0")]
     public class MagneticPickups : BaseUnityPlugin
     {
-        internal static MagneticPickups Instance;
         public static ConfigEntry<float> PickupSpeed { get; set; }
         public static ConfigEntry<float> PickupMoveUpwardsSpeed { get; set; }
         public static ConfigEntry<float> PickupRadius { get; set; }
@@ -18,23 +17,40 @@ namespace MagneticPickups
 
         public void Awake()
         {
-            Instance = this;
-
             // Set default config values, or load them.
-            MagneticPickupsConfigFile = new ConfigFile(Paths.ConfigPath + @"\MagneticPickups.cfg", true);
+            MagneticPickupsConfigFile = new ConfigFile(
+                Paths.ConfigPath + @"\MagneticPickups.cfg",
+                true
+            );
 
-            PickupSpeed = MagneticPickupsConfigFile.Bind(new ConfigDefinition("Pickups", "PickupSpeed"), 25f,
-                new ConfigDescription("The speed with which the pickups will move towards players"));
-            PickupMoveUpwardsSpeed = MagneticPickupsConfigFile.Bind(new ConfigDefinition("Pickups", "PickupMoveUpwardsSpeed"), 1.25f,
-                new ConfigDescription("The upwards velocity to add to the pickups before they start moving towards a player"));
-            PickupRadius = MagneticPickupsConfigFile.Bind(new ConfigDefinition("Pickups", "PickupRadius"), 30f,
-                new ConfigDescription("How close a pickup must be before it will begin to move towards a player"));
+            PickupSpeed = MagneticPickupsConfigFile.Bind(
+                new ConfigDefinition("Pickups", "PickupSpeed"),
+                25f,
+                new ConfigDescription("The speed with which the pickups will move towards players")
+            );
+
+            PickupMoveUpwardsSpeed = MagneticPickupsConfigFile.Bind(
+                new ConfigDefinition("Pickups", "PickupMoveUpwardsSpeed"),
+                1.25f,
+                new ConfigDescription(
+                    "The upwards velocity to add to the pickups before they start moving towards a player"
+                )
+            );
+
+            PickupRadius = MagneticPickupsConfigFile.Bind(
+                new ConfigDefinition("Pickups", "PickupRadius"),
+                50f,
+                new ConfigDescription(
+                    "How close a pickup must be before it will begin to move towards a player"
+                )
+            );
 
             Logger.LogMessage("Loaded Magnetic Pickups!");
 
             On.RoR2.GravitatePickup.FixedUpdate += (orig, self) =>
             {
                 orig.Invoke(self);
+
                 // Retrieve players and get the closest one.
                 var players = TeamComponent.GetTeamMembers(TeamIndex.Player);
                 var location = GetClosestPlayerLocation(players, self.transform.position);
@@ -42,30 +58,56 @@ namespace MagneticPickups
                 // Move the pickup towards the player's location if they are within the radius.
                 if (Vector3.Distance(self.rigidbody.position, location) < PickupRadius.Value)
                 {
-                    // Set the velocity to 0 to begin with to remove any gravity.
-                    self.rigidbody.velocity = Vector3.zero;
-
-                    // Move the pickup upwards, if it is not already high above the ground and there's nothing directly above it.
-                    bool didHitUp = Physics.Raycast(self.rigidbody.position, self.rigidbody.transform.up, out RaycastHit upwardsHit);
-                    bool didHitDown = Physics.Raycast(self.rigidbody.position, -self.rigidbody.transform.up, out RaycastHit downwardsHit);
-                    if ((!didHitUp || (didHitUp && upwardsHit.distance > 10f)) && didHitDown && downwardsHit.distance < 250f)
-                    {
-                        self.rigidbody.velocity += Vector3.up * PickupMoveUpwardsSpeed.Value;
-                    }
-
-                    var speed = Vector3.MoveTowards(self.rigidbody.velocity, (self.transform.position - location).normalized * 100f, PickupSpeed.Value);
-                    self.rigidbody.velocity += -speed;
+                    MovePickupTowardsPlayer(self, location);
                 }
             };
         }
 
-        private Vector3 GetClosestPlayerLocation(ReadOnlyCollection<TeamComponent> players, Vector3 location)
+        private void MovePickupTowardsPlayer(GravitatePickup pickup, Vector3 playerLocation)
+        {
+            // Set the velocity to 0 to begin with to remove any gravity.
+            pickup.rigidbody.velocity = Vector3.zero;
+
+            // Move the pickup upwards, if it is not already high above the ground and there's nothing directly above it.
+            var didHitUp = Physics.Raycast(
+                pickup.rigidbody.position,
+                pickup.rigidbody.transform.up,
+                out var upwardsHit
+            );
+            var didHitDown = Physics.Raycast(
+                pickup.rigidbody.position,
+                -pickup.rigidbody.transform.up,
+                out var downwardsHit
+            );
+
+            var hasSpaceUpwards = !didHitUp || upwardsHit.distance > 10f;
+            var hasSpaceDownwards = didHitDown && downwardsHit.distance < 250f;
+
+            // Only move upwards if the pickup has enough space.
+            if (hasSpaceUpwards && hasSpaceDownwards)
+            {
+                pickup.rigidbody.velocity += Vector3.up * PickupMoveUpwardsSpeed.Value;
+            }
+
+            var speed = Vector3.MoveTowards(
+                pickup.rigidbody.velocity,
+                (pickup.transform.position - playerLocation).normalized * 100f,
+                PickupSpeed.Value
+            );
+
+            pickup.rigidbody.velocity += -speed;
+        }
+
+        private Vector3 GetClosestPlayerLocation(
+            ReadOnlyCollection<TeamComponent> players,
+            Vector3 location
+        )
         {
             var closestPosition = Vector3.positiveInfinity;
             var lowestDistance = float.MaxValue;
-            foreach (TeamComponent teamComponent in players)
+            foreach (var teamComponent in players)
             {
-                NetworkUser networkBody = Util.LookUpBodyNetworkUser(teamComponent.gameObject);
+                var networkBody = Util.LookUpBodyNetworkUser(teamComponent.gameObject);
                 if (!networkBody)
                 {
                     continue;
